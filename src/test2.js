e045b8429aefc546fa4916f55ad65047f83fd6e8
Eivind Magnus Hvidevold <hvidevold@gmail.com>
2019-02-03 09:30:42 +0100

    thinking

--- src/test2.js
+++ src/test2.js
@@ -1,11 +1,33 @@
-function sum(a, b) {
+function sum_2(a, b) {
   return a + b;
 }
 
-function sum(a, b, c) {
+function sum_3(a, b, c) {
   return a + b + c;
 }
 
-function sum(a, b, c) {
+function sum_2minc(a, b, c) {
   return a + b - c;
+}
+
+function patch() {
+  JC.renameArg({
+    scope: 'sum',
+    updateCallSiteScope: '*',
+    src: 'a',
+    dst: 'addend1',
+  });
+
+  JC.renameArg({
+    scope: 'sum',
+    updateCallSiteScope: '*',
+    src: 'b',
+    dst: 'addend2'
+  });
+
+  JC.replace({
+    scope: 'sum',
+    src: 'return a + b;',
+    dst: 'return a + b + c;'
+  });
 }
